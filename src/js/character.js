f9db484bc0e9254b21a75bcb25c5a19aa39fe4a9
Olesya Zubkova <olesya_2802@mail.ru>
2021-10-26 20:51:12 +0300

    task changed

---
+++ src/js/character.js
@@ -0,0 +1,18 @@
+const allTypes = ['Bowerman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+
+export class Character {
+  constructor(name, type) {
+    if (name.length < 2 || name.length > 10) {
+      throw new Error('Неверный формат имени!');
+    }
+
+    if (!allTypes.includes(type)) {
+      throw new Error('Неверный формат типа!');
+    }
+
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+  }
+}
